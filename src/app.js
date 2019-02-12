e6a466361d032b9a1c19d9826661410c703863f9
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2019-02-12 16:28:43 -0500

    Using babel to auto generate JSX from app.js  to scripts\app.js

--- src/app.js
+++ src/app.js
@@ -1,13 +1,7 @@
-"use strict";
-
 console.log("App.js is running!");
 
 // JSX - JavaScript XML
-var template = React.createElement(
-  "h1",
-  null,
-  "Indecision App"
-);
+var template = <h1>Indecision App</h1>
 var appRoot = document.getElementById("app");
 
 // render() method is using to render the jsx code on the elmeent 
