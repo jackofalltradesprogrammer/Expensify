7df878831cce918802900fc2e6cebf1b93ff2df8
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2019-02-12 17:36:20 -0500

    intro to react and JSX rendering

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,11 +4,55 @@ console.log("App.js is running!");
 
 // JSX - JavaScript XML
 var template = React.createElement(
-  "h1",
-  null,
-  "Indecision App"
+    "div",
+    null,
+    React.createElement(
+        "h1",
+        null,
+        "Indecision App"
+    ),
+    React.createElement(
+        "p",
+        null,
+        "This is some info"
+    ),
+    React.createElement(
+        "ol",
+        null,
+        React.createElement(
+            "li",
+            null,
+            "Item one"
+        ),
+        React.createElement(
+            "li",
+            null,
+            "Item two"
+        )
+    )
+);
+
+// Create a templateTwo var JSX expression
+var templateTwo = React.createElement(
+    "div",
+    null,
+    React.createElement(
+        "h1",
+        null,
+        "Harpreet"
+    ),
+    React.createElement(
+        "p",
+        null,
+        "Age: 29"
+    ),
+    React.createElement(
+        "p",
+        null,
+        "Location: New York"
+    )
 );
 var appRoot = document.getElementById("app");
 
-// render() method is using to render the jsx code on the elmeent 
-ReactDOM.render(template, appRoot);
+// render() method is using to render the jsx code on the elmeent
+ReactDOM.render(templateTwo, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,8 +1,28 @@
 console.log("App.js is running!");
 
 // JSX - JavaScript XML
-var template = <h1>Indecision App</h1>
+var template = (
+  <div>
+    <h1>Indecision App</h1>
+    <p>This is some info</p>
+    <ol>
+        <li>Item one</li>
+        <li>Item two</li>
+    </ol>
+
+  </div>
+);
+
+// Create a templateTwo var JSX expression
+var templateTwo =(
+    <div>
+        <h1>Harpreet</h1>
+        <p>Age: 29</p>
+        <p>Location: New York</p>
+
+    </div>
+);
 var appRoot = document.getElementById("app");
 
-// render() method is using to render the jsx code on the elmeent 
-ReactDOM.render(template, appRoot);
+// render() method is using to render the jsx code on the elmeent
+ReactDOM.render(templateTwo, appRoot);
