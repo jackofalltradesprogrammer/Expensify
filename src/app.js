5a6555b89e66dca8ce3b2665886a1307d49394a2
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2019-02-12 21:43:56 -0500

    change all var to const or let

--- src/app.js
+++ src/app.js
@@ -8,13 +8,13 @@ console.log("App.js is running!");
 // render new p tag - if options.length > 0 "Here are your options" "No options"
 
 // create app object title/subtile
-var app = {
+const app = {
   title: "Indecision App",
   subtitle: "Ichanged it",
   options: ['One', 'Two']
 };
 // JSX - JavaScript XML
-var template = (
+const template = (
   <div>
     <h1>{app.title}</h1>
     { app.subtitle && <p>{app.subtitle}</p>}
@@ -26,7 +26,7 @@ var template = (
   </div>
 );
 
-var user = {
+const user = {
     name: 'Andrew',
     age: 26,
     location: 'Philadelphia'
@@ -39,14 +39,14 @@ function getLocation(location) {
   // it returns undefined if nothing is supplied, we can use JSX to
 }
 // Create a templateTwo var JSX expression
-var templateTwo = (
+const templateTwo = (
   <div>
     <h1>{user.name ? user.name : 'Anonymous'}</h1>
     {(user.age && user.age >= 18) && <p>Age: {user.age}</p>}
-    {getLocation()}
+    {getLocation(user.location)}
   </div>
 );
-var appRoot = document.getElementById("app");
+const appRoot = document.getElementById("app");
 
 // render() method is using to render the jsx code on the elmeent
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
