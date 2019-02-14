a19d4432c913c4044bef16702434b259a17f2937
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2019-02-13 20:54:47 -0500

    17. Manual data binding with rendering the dom

--- src/app.js
+++ src/app.js
@@ -28,28 +28,38 @@ const template = (
 
 let count = 0;
 const addOne = () => {
-  count = count + 1;
-  console.log("addOne");
+  count++;
+  renderCounterApp();
 };
 const minusOne = () => {
-  count = count -1;
-  console.log("minusOne");
+  count--;
+  renderCounterApp();
 };
 const reset = () => {
   console.log("Reset Count");
   count = 0;
+  renderCounterApp();
 };
 // class attribute from html is called className in JSX as it is reserved
-const templateTwo = (
-  <div>
-    <h1>Count: {count}</h1>
-    <button onClick={addOne}>+1</button>
-    <button onClick={minusOne}>-1</button>
-    <button onClick={reset}>reset</button>
-  </div>
-);
+// JSX don't have data binding 
+
 
 const appRoot = document.getElementById("app");
 
 // render() method is using to render the jsx code on the elmeent
-ReactDOM.render(templateTwo, appRoot);
+
+
+const renderCounterApp = () => {
+  const templateTwo = (
+    <div>
+      <h1>Count: {count}</h1>
+      <button onClick={addOne}>+1</button>
+      <button onClick={minusOne}>-1</button>
+      <button onClick={reset}>reset</button>
+    </div>
+  );
+
+  ReactDOM.render(templateTwo, appRoot);
+};
+
+renderCounterApp();
