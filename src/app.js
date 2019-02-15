c7fe5299fc9e61dc890f5ce97a765eda0b2737e8
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2019-02-15 14:10:05 -0500

    28. Component props

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
 // react uses the first letter of the component to differentiate from HTML
 class IndecisionApp extends React.Component {
   render() {
+    const title = "Indecision";
+    const subtitle = "Put your life in the hands of a computer";
+    const options = ["Thing one", "Thing two", "Thing three", "Thing four"];
+
     return (
       <div>
-        <Header />
+        <Header title={title} subtitle={subtitle} />
         <Action />
-        <Options />
+        <Options options={options} />
         <AddOption />
       </div>
     );
@@ -13,10 +17,11 @@ class IndecisionApp extends React.Component {
 }
 class Header extends React.Component {
   render() {
+    console.log(this.props);
     return (
       <div>
-        <h1>Indecision</h1>
-        <h2>Put your life in the hands of a computer</h2>
+        <h1>{this.props.title}</h1>
+        <h2>{this.props.subtitle}</h2>
       </div>
     );
   }
@@ -33,12 +38,16 @@ class Action extends React.Component {
 }
 
 // * Options -> Options Component here
+// todo: Setup options prop form Options component
+// todo: Render the length of the array
+
 class Options extends React.Component {
   render() {
     return (
       <div>
-        Options Component here
-        <Option />
+        {this.props.options.map(option => (
+          <Option key={option} optionText={option} />
+        ))}
       </div>
     );
   }
@@ -47,24 +56,15 @@ class Options extends React.Component {
 // * AddOption -> Add Option component here
 class AddOption extends React.Component {
   render() {
-    return (
-      <div>
-        AddOption Component here
-      </div>
-    )
+    return <div>AddOption Component here</div>;
   }
 }
 
 // * Option -> Option component here
 class Option extends React.Component {
   render() {
-    return (
-      <div>
-        Option Component here
-      </div>
-    );
+    return <div>{this.props.optionText}</div>;
   }
 }
-
 
 ReactDOM.render(<IndecisionApp />, document.getElementById("app"));
