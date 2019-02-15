d40f1b840381e1a45082a8f8a6c20d3775b53dfa
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2019-02-15 16:48:02 -0500

    32. Adding State to Counter App: part 1

--- src/playground/counter-example.js
+++ src/playground/counter-example.js
@@ -1,37 +1,67 @@
-let count = 0;
-const addOne = () => {
-  count++;
-  renderCounterApp();
-};
-const minusOne = () => {
-  count--;
-  renderCounterApp();
-};
-const reset = () => {
-  console.log("Reset Count");
-  count = 0;
-  renderCounterApp();
-};
-// class attribute from html is called className in JSX as it is reserved
-// JSX don't have data binding 
+// * todo: Create three methods: handleAddOne, handleMinusOne, handleReset
+// * todo: Use console.log to print method name
+// * todo: Wire up onClick & bind in the constructor
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleAddOne = this.handleAddOne.bind(this);
+    this.handleMinusOne = this.handleMinusOne.bind(this);
+    this.handleReset = this.handleReset.bind(this);
+  }
+  handleAddOne() {
+    console.log('handleAddOne');
+  }
+  handleMinusOne() {
+    console.log('handleMinusOne');
+  }
+  handleReset() {
+    console.log('handleReset');
+  }
+  render() {
+    return (
+      <div>
+        <h1>Count: </h1>
+        <button onClick={this.handleAddOne }>+1</button>
+        <button onClick={this.handleMinusOne}>-1</button>
+        <button onClick={this.handleReset}>reset</button>
+      </div>
+    );
+  }
+}
 
+ReactDOM.render(<Counter />, document.getElementById('app'));
+// let count = 0;
+// const addOne = () => {
+//   count++;
+//   renderCounterApp();
+// };
+// const minusOne = () => {
+//   count--;
+//   renderCounterApp();
+// };
+// const reset = () => {
+//   console.log("Reset Count");
+//   count = 0;
+//   renderCounterApp();
+// };
+// // class attribute from html is called className in JSX as it is reserved
+// // JSX don't have data binding
 
-const appRoot = document.getElementById("app");
+// const appRoot = document.getElementById("app");
 
-// render() method is using to render the jsx code on the elmeent
+// // render() method is using to render the jsx code on the elmeent
 
+// const renderCounterApp = () => {
+//   const templateTwo = (
+//     <div>
+//       <h1>Count: {count}</h1>
+//       <button onClick={addOne}>+1</button>
+//       <button onClick={minusOne}>-1</button>
+//       <button onClick={reset}>reset</button>
+//     </div>
+//   );
 
-const renderCounterApp = () => {
-  const templateTwo = (
-    <div>
-      <h1>Count: {count}</h1>
-      <button onClick={addOne}>+1</button>
-      <button onClick={minusOne}>-1</button>
-      <button onClick={reset}>reset</button>
-    </div>
-  );
+//   ReactDOM.render(templateTwo, appRoot);
+// };
 
-  ReactDOM.render(templateTwo, appRoot);
-};
-
-renderCounterApp();
+// renderCounterApp();
