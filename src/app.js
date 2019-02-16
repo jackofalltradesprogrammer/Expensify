edf797fef5afb273b5e7715d1de51926bc114bd0
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2019-02-16 16:45:58 -0500

    51. Es6 import/export- named

--- src/app.js
+++ src/app.js
@@ -1,1 +1,12 @@
-console.log('app.js is running');
+// import './utils.js';
+// named imports - name should match
+// import { square, add } from './utils.js';
+
+// console.log('app.js is running');
+// console.log(square(4));
+// console.log(add(100, 23));
+
+import {isAdult, canDrink} from './person';
+
+console.log('you are an adult ', isAdult(18));
+console.log('you can drink', canDrink(18));
---
+++ src/utils.js
@@ -0,0 +1,9 @@
+console.log('utils.js is running');
+
+export const square = (x) => x*x;
+export const add = (a, b) => a+b;
+
+// export { square, add }; 
+// we can use export keyword as defined above to export functions
+
+// ! exports - default export - named exports;
