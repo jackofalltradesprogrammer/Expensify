45d0d3292d600309ccfdb03ce8ada7e2aad2bc57
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2019-02-16 17:36:33 -0500

    53. Import npm modules React, ReactDOM

--- src/app.js
+++ src/app.js
@@ -1,18 +1,6 @@
-// import './utils.js';
-// ! named imports - name should match
-// ! default export don't have curly braces and you can call it anyhting
-// import subtractAnyname, { square, add } from './utils.js';
+// install --> import use
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-// console.log('app.js is running');
-// console.log(square(4));
-// console.log(add(100, 23));
-// console.log(subtractAnyname(100, 23));
-
-
-import isSenior, {isAdult, canDrink} from './person';
-
-console.log('you are an adult ', isAdult(18));
-console.log('you can drink', canDrink(18));
-console.log('you are a senior', isSenior(65));
-
-
+const template = React.createElement('p', {}, 'testing 123');
+ReactDOM.render(template, document.getElementById('app'));
