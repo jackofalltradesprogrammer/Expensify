31e98204b4c2c14c08913742b44e4309d167677a
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2019-02-16 17:05:59 -0500

    52. Default Exports - import can be named anything

--- src/app.js
+++ src/app.js
@@ -1,12 +1,18 @@
 // import './utils.js';
-// named imports - name should match
-// import { square, add } from './utils.js';
+// ! named imports - name should match
+// ! default export don't have curly braces and you can call it anyhting
+// import subtractAnyname, { square, add } from './utils.js';
 
 // console.log('app.js is running');
 // console.log(square(4));
 // console.log(add(100, 23));
+// console.log(subtractAnyname(100, 23));
 
-import {isAdult, canDrink} from './person';
+
+import isSenior, {isAdult, canDrink} from './person';
 
 console.log('you are an adult ', isAdult(18));
 console.log('you can drink', canDrink(18));
+console.log('you are a senior', isSenior(65));
+
+
