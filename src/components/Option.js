d0cd59750bb44012ceeb646eb860705428ec8da8
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2019-02-18 14:22:58 -0500

    70.  Styling Option and Add options Errors

--- src/components/Option.js
+++ src/components/Option.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
 const Option = props => (
-  <div>
-    {props.optionText}
-    <button 
+  <div className="option">
+    <p className="option__text">
+      {props.count}.{props.optionText}
+    </p>
+    <button
       className="button button--link"
       onClick={e => {
         props.handleDeleteOption(props.optionText);
