07c037d12ff8cb1863320b03798a9389c380140c
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2019-02-17 20:41:03 -0500

    62. Refactoring other stateless funcComp with noReturn  arrow form

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,15 @@
 import React from 'react';
 
-const Header = (props) => {
-    return (
-      <div>
-        <h1>{props.title}</h1>
-        {props.subtitle &&<h2> {props.subtitle}</h2>}
-      </div>
-    );
-  };
-  
-  // ! to pass functional component default props 
-  Header.defaultProps = {
-    title: 'Indecision'
-  };
-  
-  export default Header;
+const Header = props => (
+  <div>
+    <h1>{props.title}</h1>
+    {props.subtitle && <h2> {props.subtitle}</h2>}
+  </div>
+);
+
+// ! to pass functional component default props
+Header.defaultProps = {
+  title: 'Indecision'
+};
+
+export default Header;
