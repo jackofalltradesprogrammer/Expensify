eaf6d35b0b3c3047d5d9295d35c6862ef471c33e
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2019-02-18 12:59:35 -0500

    68. Big Button & options list - styling  and using BEM modifier

--- src/components/Action.js
+++ src/components/Action.js
@@ -3,7 +3,7 @@ import React from 'react';
 // ! In stateless functions props needs to be passed as arguments
 const Action = props => (
   <div>
-    <button onClick={props.handlePick} disabled={!props.hasOptions}>
+    <button className="big-button" onClick={props.handlePick} disabled={!props.hasOptions}>
       What Should I do?
     </button>
   </div>
