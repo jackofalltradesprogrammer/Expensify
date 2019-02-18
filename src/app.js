c52c623a41d6fb4271eb2fe0b29827d6d1213c9f
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2019-02-18 10:46:23 -0500

    64. Setting up webback with css part 1

--- src/app.js
+++ src/app.js
@@ -2,27 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp';
+import './styles/styles.css';
 
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
 
-// const Layout = (props) => {
-//   return (
-//     <div>
-//       <p>header</p>
-       // ! props.children get all JSX passed between the parent tags
-//       {props.children}
-//       <p>footer</p>
-//     </div>
-//   );
-// };
-
-// ReactDOM.render((
-//   <Layout>
-//     <div>
-//       <h1>Page Title</h1>
-//       <p>This is my page</p>
-//     </div>
-//   </Layout>
-//   ), document.getElementById('app'));
-// one way to pass jsx
-// ReactDOM.render(<Layout content={template} />, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -3,27 +3,35 @@ const path = require('path');
 
 // config file needs an absolute path that why we are using node path module
 module.exports = {
-    entry: './src/app.js',
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js'
-    },
-    // module has rules array to tell what it should do
-    module: {
-        rules: [{
-            loader: 'babel-loader',
-            test: /\.js$/, // regex to tell when ever you see .js files run babel
-            exclude: /node_modules/ // don't include js files in node_modules
-        }]
-    },
-    // ! this helps to find the original location of the error in console
-    devtool: 'cheap-module-eval-source-map' ,
-    // ! devServer takes incharge of building for webpack while giving the server abilities
-    // ! it puts the bundle.js in the memory for fast loading 
-    devServer: {
-        contentBase: path.join(__dirname, 'public'),
-    }
+  entry: './src/app.js',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  // module has rules array to tell what it should do
+  module: {
+    rules: [
+      {
+        loader: 'babel-loader',
+        test: /\.js$/, // regex to tell when ever you see .js files run babel
+        exclude: /node_modules/ // don't include js files in node_modules
+      },
+      {
+        test: /\.css$/, // regext to tell get .css files
+        // ! CSS Loader convers css into javascript - Style Loader takes javascript css and adds to the dom with <style> tag
+        // to provide arrays of loaders
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  // ! this helps to find the original location of the error in console
+  devtool: 'cheap-module-eval-source-map',
+  // ! devServer takes incharge of building for webpack while giving the server abilities
+  // ! it puts the bundle.js in the memory for fast loading
+  devServer: {
+    contentBase: path.join(__dirname, 'public')
+  }
 };
 
 // * loader - a file gets transformed when webpack uses it
-// 
+//
