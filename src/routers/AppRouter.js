f2960604f4a823269e13f9e03c2eda0049f3f4eb
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2019-02-22 13:35:19 -0500

    setup for portfolio site - Router

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,9 +1,5 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import ExpenseDashboardPage  from '../components/ExpenseDashboardpage';
-import AddExpensePage  from '../components/AddExpensePage';
-import EditExpensePage from '../components/EditExpensePage';
-import HelpPage from '../components/HelpPage';
 import Header from '../components/Header';
 import NotFoundPage from '../components/NotFoundPage';
 
@@ -12,12 +8,6 @@ const AppRouter = () => (
     <div>
       <Header />
       <Switch>
-        {/*-- Route render if the first character of url matches and to override that 'exact' match needs to be true.
-      ':' is used to make it dynamice and grab that value */}
-        <Route path="/" component={ExpenseDashboardPage} exact={true} />
-        <Route path="/create" component={AddExpensePage} />
-        <Route path="/edit/:id" component={EditExpensePage} />
-        <Route path="/help" component={HelpPage} />
         <Route component={NotFoundPage} />
       </Switch>
     </div>
@@ -25,3 +15,4 @@ const AppRouter = () => (
 );
 
 export default AppRouter;
+s
