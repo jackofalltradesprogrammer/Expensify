f41d3decf08b1b7cd7dbcbb4d13c9a5505dd1b1c
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2019-02-23 10:11:32 -0500

    86. Dispatching Actions  in REDUX - createStore() runs again

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,8 +1,47 @@
-import {createStore} from 'redux';
+import { createStore } from 'redux';
 
 // create store in Redux and it takes a previous state object
-const store = createStore((state = { count: 0 }) => {
-  return state;
+// whenever something is dispatched createStore runs again
+// as we add more dispatch calls count keeps performing the action given
+const store = createStore((state = { count: 0 }, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return {
+        count: state.count + 1
+      };
+    case 'DECREMENT':
+      return {
+        count: state.count - 1
+      };
+    case 'RESET':
+      return {
+        count: state.count = 0
+      };
+    default:
+      return state;
+  }
+});
+
+console.log(store.getState());
+
+// TODO:  Actions - than an object that gets sent to the store
+
+// TODO: increment, decrement, reset
+// whenever something is dispatched createStore runs again
+// as we add more dispatch calls count keeps performing the action given
+store.dispatch({
+  type: 'INCREMENT'
+});
+
+
+// TODO: i'd like to reset the count to zero
+
+store.dispatch({
+  type: 'RESET'
+});
+
+store.dispatch({
+  type: 'DECREMENT'
 });
 
 console.log(store.getState());
