354eeeb77b2728130a77e675dad8251256f5e11f
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2019-02-23 10:37:28 -0500

    87. Subscribing - to state and Dynamic actions to change state

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -6,42 +6,56 @@ import { createStore } from 'redux';
 const store = createStore((state = { count: 0 }, action) => {
   switch (action.type) {
     case 'INCREMENT':
+      const incrementBy =
+        typeof action.incrementBy === 'number' ? action.incrementBy : 1;
       return {
-        count: state.count + 1
+        count: state.count + incrementBy
       };
     case 'DECREMENT':
+      const decrementBy =
+        typeof action.decrementBy === 'number' ? action.decrementBy : 1;
       return {
-        count: state.count - 1
+        count: state.count - decrementBy
+      };
+    case 'SET':
+      return {
+        count: action.count
       };
     case 'RESET':
       return {
-        count: state.count = 0
+        count: (state.count = 0)
       };
     default:
       return state;
   }
 });
 
-console.log(store.getState());
-
-// TODO:  Actions - than an object that gets sent to the store
-
-// TODO: increment, decrement, reset
-// whenever something is dispatched createStore runs again
-// as we add more dispatch calls count keeps performing the action given
-store.dispatch({
-  type: 'INCREMENT'
+// to keep a watch on the store - the Redux store gives us subscription features
+// The unsubscription feature is also available 'subscribe()' functions returns a functions
+// and we can call it to cancel the subscription
+const unsubscribe = store.subscribe(() => {
+  console.log(store.getState());
 });
 
+// we can pass dynamic information with the dispatch calls
+store.dispatch({
+  type: 'INCREMENT',
+  incrementBy: 5
+});
 
-// TODO: i'd like to reset the count to zero
+// call the unsubscribe function
+// unsubscribe();
 
 store.dispatch({
   type: 'RESET'
 });
 
 store.dispatch({
-  type: 'DECREMENT'
+  type: 'DECREMENT',
+  decrementBy: 10
 });
 
-console.log(store.getState());
+store.dispatch({
+  type: 'SET',
+  count: 10
+});
