a417967e07f0624dae67d962e06a7f8ed3a8e4b2
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2019-02-23 12:09:52 -0500

    removing extra comments

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,20 +1,5 @@
 import { createStore } from 'redux';
 
-// Action generators  - functions that return action objects
-// It helps to avoid type errors as well
-// We can also destructure arguments passed in the functions
-
-const add = ({ a, b }, c) => {
-  return a + b + c;
-};
-console.log(add({ a: 1, b: 12 }, 100));
-
-// we will do destrucuring on the arguments payload()
-// const incrementCount = (payload = {}) => ({
-//   type: 'INCREMENT',
-//   incrementBy: typeof payload.incrementBy === 'number' ? payload.incrementBy : 1
-// });
-// we use destructuring and also set the value of argument as default to be empty
 const incrementCount = ({ incrementBy = 1 } = {}) => ({
   type: 'INCREMENT',
   // Use es6 to set the value if names are same
@@ -32,9 +17,6 @@ const setCount = ({ count }) => ({ type: 'SET', count });
 // reset Count
 const resetCount = () => ({ type: 'RESET' });
 
-// create store in Redux and it takes a previous state object
-// whenever something is dispatched createStore runs again
-// as we add more dispatch calls count keeps performing the action given
 const store = createStore((state = { count: 0 }, action) => {
   switch (action.type) {
     case 'INCREMENT':
@@ -58,18 +40,9 @@ const store = createStore((state = { count: 0 }, action) => {
   }
 });
 
-// to keep a watch on the store - the Redux store gives us subscription features
-// The unsubscription feature is also available 'subscribe()' functions returns a functions
-// and we can call it to cancel the subscription
 const unsubscribe = store.subscribe(() => {
   console.log(store.getState());
 });
-
-// we can pass dynamic information with the dispatch calls
-// store.dispatch({
-//   type: 'INCREMENT',
-//   incrementBy: 5
-// });
 
 store.dispatch(incrementCount({ incrementBy: 5 }));
 
@@ -77,14 +50,7 @@ store.dispatch(incrementCount({ incrementBy: 5 }));
 // unsubscribe();
 
 store.dispatch(incrementCount());
-
 store.dispatch(resetCount());
-
-// store.dispatch({
-//   type: 'DECREMENT',
-//   decrementBy: 10
-// });
 store.dispatch(decrementCount());
 store.dispatch(decrementCount({ decrementBy: 10 }));
-
 store.dispatch(setCount({ count: 101 }));
