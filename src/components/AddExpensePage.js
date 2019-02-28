de17831b585bfdb94ec4a053deb214720e82de98
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2019-02-28 12:10:22 -0500

    105. Creating Expensense Form with regex

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import ExpenseForm from './ExpenseForm';
 
 const AddExpensePage = () => (
-  <div>This is from my add expense component</div>
+  <div>
+    <h1>Add Expense</h1>
+    <ExpenseForm />
+  </div>
 );
 
 export default AddExpensePage;
