dbe8a5e39adbe01e4ccd6eacee73d26de18e3bd7
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2019-03-03 11:54:43 -0500

    120. Snapshot Testing with Dynamic Components  with data and no data

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import ExpenseListItem  from './ExpenseListItem';
+import ExpenseListItem from './ExpenseListItem';
 import selectExpenses from '../selectors/expenses';
 // Connect is used to read from the store
 
-const ExpenseList = props => (
+export const ExpenseList = props => (
   <div>
-    <h1>ExpenseList</h1>
-    {props.expenses.map((expense) => {
-      return <ExpenseListItem key={expense.id} {...expense} />;
-    })}
-    
+    {props.expenses.length === 0 ? (
+      <p> No expenses</p>
+    ) : (
+      props.expenses.map(expense => {
+        return <ExpenseListItem key={expense.id} {...expense} />;
+      })
+    )}
   </div>
 );
 
