92a84dc1122429e2e3bfa2c28b64c8c13bd625b1
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2019-03-03 13:41:02 -0500

    122. Testing user interaction - simulate events, compare state values

--- src/tests/components/ExpenseForm.test.js
+++ src/tests/components/ExpenseForm.test.js
@@ -13,3 +13,57 @@ test('should render ExpenseForm correctly with expense data', () => {
   const wrapper = shallow(<ExpenseForm expense={expenses[1]} />);
   expect(wrapper).toMatchSnapshot();
 });
+
+test('should render an error for invalid form submission', () => {
+  const wrapper = shallow(<ExpenseForm />);
+  expect(wrapper).toMatchSnapshot();
+  // .find helps to find an element ( arguments can be classnames, ids etc)
+  // ! As we are simulating, we are not passing any date with it
+  // ! And in our onSubmit() we are calling prevent default function on 'e' which generates an error as it is undefined
+  wrapper.find('form').simulate('submit', {
+    preventDefault: () => {}
+  });
+  // wrapper.state, is used to access the state of the component
+  expect(wrapper.state('error').length).toBeGreaterThan(0);
+  expect(wrapper).toMatchSnapshot();
+});
+
+test('should set description on input change', () => {
+  const value = 'New description';
+  const wrapper = shallow(<ExpenseForm />);
+  // if multiple elemeents are there, 'at' is used to find the position by providing the index
+  wrapper.find('input').at(0).simulate('change', {
+    target: {value}
+  });
+  expect(wrapper.state('description')).toBe(value);
+});
+
+// TODO should set note on textarea change
+test('should set note on textarea change', () => {
+  const value = 'New note';
+  const wrapper = shallow(<ExpenseForm />);
+  wrapper.find('textarea').simulate('change', {
+    target: {value}
+  });
+  expect(wrapper.state('note')).toBe(value);
+});
+
+// TODO should set amount if valid input 
+test('should set amount if valid input', () => {
+  const value = '23.50';
+  const wrapper = shallow(<ExpenseForm />);
+  wrapper.find('input').at(1).simulate('change', {
+    target: {value}
+  });
+  expect(wrapper.state('amount')).toBe(value);
+});
+
+// TODO should not set amount if invalid input
+test('should not set amount if invalid input', () => {
+  const value = '12.122';
+  const wrapper = shallow(<ExpenseForm />);
+  wrapper.find('input').at(1).simulate('change', {
+    target: {value}
+  });
+  expect(wrapper.state('amount')).toBe('');
+});
