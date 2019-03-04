bf8771f6e52a244be1fa3ba4ccb054fa6b6d2ecb
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2019-03-04 17:05:53 -0500

    143. Writing to the Firebase database - using ref('name') set('value')

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,16 +1,35 @@
 // it talkes all non-named exports and dumb them in firebase
-import * as firebase from 'firebase'; 
+import * as firebase from 'firebase';
 
 const config = {
-  apiKey: "AIzaSyD94FKlyXjiSD7-eHrvFi-SqrT-w-HmSIo",
-  authDomain: "expensify-c466a.firebaseapp.com",
-  databaseURL: "https://expensify-c466a.firebaseio.com",
-  projectId: "expensify-c466a",
-  storageBucket: "expensify-c466a.appspot.com",
-  messagingSenderId: "39226797095"
+  apiKey: 'AIzaSyD94FKlyXjiSD7-eHrvFi-SqrT-w-HmSIo',
+  authDomain: 'expensify-c466a.firebaseapp.com',
+  databaseURL: 'https://expensify-c466a.firebaseio.com',
+  projectId: 'expensify-c466a',
+  storageBucket: 'expensify-c466a.appspot.com',
+  messagingSenderId: '39226797095'
 };
 firebase.initializeApp(config);
 
-firebase.database().ref().set({
-  name: 'Harpreet Singh'
+const database = firebase.database();
+
+// ref() - It gives us a reference to the subset of a database
+// if we don't pass anything we get the access to the root of the database
+database.ref().set({
+  name: 'Harpreet Singh',
+  age: 26,
+  isSingle: true,
+  location: {
+    city: 'Philadelphia',
+    country: 'United States'
+  }
 });
+
+// set can take anytime of data supplied to it
+// database.ref().set('This is my data.');
+
+// if no arguments supplied to ref, it erases everything
+database.ref('age').set(27);
+database.ref('location/city').set('New York');
+// attributes object with height and weight
+database.ref('attributes').set({ height: 66, weight: 150 });
