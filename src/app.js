7cf6e824b351552403de0a2dddd147ca529716f6
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2019-03-08 15:47:28 -0500

    128 to 132 -  Git SSH Github

--- src/app.js
+++ src/app.js
@@ -14,12 +14,12 @@ import './firebase/firebase';
 // get access to Redux store for dispatching actions and state etc.
 const store = configureStore();
 
-// TODO addExpense -> Water bill
-store.dispatch(addExpense({ description: 'Water bill', amount: 4500 }));
-// TODO addExpense -> Gas bill
-store.dispatch(addExpense({ description: 'Gas bill', createdAt: 1000 }));
-store.dispatch(addExpense({ description: 'Rent', amount: 109500 }));
-// TODO setTextFilter -> bill (2 items) -> water (1 item)
+// // TODO addExpense -> Water bill
+// store.dispatch(addExpense({ description: 'Water bill', amount: 4500 }));
+// // TODO addExpense -> Gas bill
+// store.dispatch(addExpense({ description: 'Gas bill', createdAt: 1000 }));
+// store.dispatch(addExpense({ description: 'Rent', amount: 109500 }));
+// // TODO setTextFilter -> bill (2 items) -> water (1 item)
 
 // As the Redux store changes, the component renders itself
 // setTimeout(() => {
@@ -27,11 +27,11 @@ store.dispatch(addExpense({ description: 'Rent', amount: 109500 }));
 // }, 3000);
 
 // TODO getVisibleExpenses -> print visibles ones to screen
-const visibleExpenses = getVisibleExpenses(
-  store.getState().expenses,
-  store.getState().filters
-);
-console.log(visibleExpenses);
+// const visibleExpenses = getVisibleExpenses(
+//   store.getState().expenses,
+//   store.getState().filters
+// );
+// console.log(visibleExpenses);
 
 const jsx = (
   <Provider store={store}>
