b4cdecc0660be7ef6a36da39bd1f7885754353d4
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2019-03-08 17:01:16 -0500

    134. Creating Seperate CSS files with Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,17 @@
 // entry  -> output where do we put it
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 // config file needs an absolute path that why we are using node path module
 // we converted the object into function because webpack calls this function with env argument
 // and we can set that one to production in the package.json file
-module.exports = (env) => {
+module.exports = env => {
   // we use this determine what type of webPack sourceMap we need to use
   const isProduction = env === 'production';
+
+  // the instance to get the CSS out of the bundle.js
+  const CSSExtract = new ExtractTextPlugin('syles.css');
+
   console.log('env', env);
   return {
     entry: './src/app.js',
@@ -27,12 +32,30 @@ module.exports = (env) => {
           // ! CSS Loader convers css into javascript - Style Loader takes javascript css and adds to the dom with <style> tag
           // to provide arrays of loaders
           // ! sass-loader uses node-sass behinde the scenese to convert scss into css
-          use: ['style-loader', 'css-loader', 'sass-loader']
+          use: CSSExtract.extract({
+            use: [
+              {
+                // we provide an object for loader so that we can pass the options which will help set sourceMap to true
+                loader: 'css-loader',
+                options: {
+                  sourceMap: true
+                }
+              },
+              {
+                loader: 'sass-loader',
+                options: {
+                  sourceMap: true
+                }
+              }
+            ]
+          })
         }
       ]
     },
+    plugins: [CSSExtract],
     // ! this helps to find the original location of the error in console
-    devtool: isProduction? 'source-map' : 'cheap-module-eval-source-map',
+    // we are changing the sourceMap for dev as the CSS source map are not displaying correctly
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
     // ! devServer takes incharge of building for webpack while giving the server abilities
     // ! it puts the bundle.js in the memory for fast loading
     devServer: {
