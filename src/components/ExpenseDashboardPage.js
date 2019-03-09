59b834299f76b641dd74b703406c94f1dbc797ff
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2019-03-08 20:36:07 -0500

    error fix

--- src/components/ExpenseDashboardPage.js
+++ src/components/ExpenseDashboardPage.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ExpenseList from './ExpenseList';
 import ExpenseListFilters from './ExpenseListFilters';
 
-const ExpensesDashboardPage = () => (
+const ExpenseDashboardPage = () => (
   <div>
     <ExpenseListFilters />
     <ExpenseList />
   </div>
 );
 
-export default ExpensesDashboardPage;
+export default ExpenseDashboardPage;
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import ExpensesDashboardPage  from '../components/ExpensesDashboardPage';
 import AddExpensePage  from '../components/AddExpensePage';
+import ExpenseDashboardPage from '../components/ExpenseDashboardPage'
 import EditExpensePage from '../components/EditExpensePage';
 import HelpPage from '../components/HelpPage';
 import Header from '../components/Header';
@@ -14,7 +14,7 @@ const AppRouter = () => (
       <Switch>
         {/*-- Route render if the first character of url matches and to override that 'exact' match needs to be true.
       ':' is used to make it dynamice and grab that value */}
-        <Route path="/" component={ExpensesDashboardPage} exact={true} />
+        <Route path="/" component={ExpenseDashboardPage} exact={true} />
         <Route path="/create" component={AddExpensePage} />
         <Route path="/edit/:id" component={EditExpensePage} />
         <Route path="/help" component={HelpPage} />
