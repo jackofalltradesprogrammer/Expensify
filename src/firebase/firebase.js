2089086a1840642bfffee205271c320779d2e5e1
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2019-03-11 15:43:08 -0400

    Array Data in Firebase: part 1 using push()

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -13,88 +13,63 @@ firebase.initializeApp(config);
 
 const database = firebase.database();
 
-// on() is similar to once() but it's like a subscription and takes a callback function
-// we cannot use promises here becuase promises need to be resolved and we need functions to execute when data changes
-// it returns the callback function  unmodified to pass to off (subscription is cancelled)
-const onValueChange = database.ref().on('value', (snapshot) => {
-  console.log(snapshot.val());
-}, (e) => {
-  console.log('Error with data fetching', e);
+// TODO Setup "expenses" with three items (description, note, amount, createAt)
+database.ref('expenses').push({
+  description: 'Rent',
+  note: '',
+  amount: 19500,
+  createdAt: 983768768
+});
+database.ref('expenses').push({
+  description: 'Phone bill',
+  note: '',
+  amount: 5900,
+  createdAt: 932432383
+});
+database.ref('expenses').push({
+  description: 'Food',
+  note: '',
+  amount: 9195,
+  createdAt: 993454983
 });
 
-setTimeout(() => {
-  database.ref('age').set(29);
-}, 3500);
-
-setTimeout(() => {
-  database.ref().off('value',onValueChange);
-}, 7000);
-
-
-setTimeout(() => {
-  database.ref('age').set(30);
-}, 10500);
-
-// Setup data sub -> Andrew is a software Developer at amazon
-const onValueChange2 = database.ref().on('value', (snapshot) => {
-  const value = snapshot.val();
-  console.log(`${value.name} is a ${value.job.title} at ${value.location.city}` );
-}, (e) => {
-  console.log('Data Fetching failed', e);
-});
-
-setTimeout(() => {
-  database.ref().update({
-    name: 'Harpreet'
-  });
-}, 3500);
-// how to fetch data from firebase database and only fetches information once
-// database
-//   .ref('location/city')
-//   .once('value')  // once takes the eventType - so we pass value
-//   .then((snapshot) => {
-//     const val = snapshot.val();
-//     console.log(val);
-//   }).catch((e) => {
-//     console.log('Error fetching data', e);
-//   });
-
-// ref() - It gives us a reference to the subset of a database
-// if we don't pass anything we get the access to the root of the database
-// database
-//   .ref()
-//   .set({
-//     name: 'Harpreet Singh',
-//     age: 26,
-//     stressLevel: 6,
-//     job: { title: 'Software developer', company: 'Google' },
-//     location: {
-//       city: 'Philadelphia',
-//       country: 'United States'
-//     }
-//   })
-//   .then(() => {
-//     console.log('Data is saved');
-//   })
-//   .catch(e => {
-//     console.log('This failed.', e);
-//   });
-
-// we use object as an argument for update() to make the change; its different from set where you can pass anything
-// we can add or delete by setting null.
-// database.ref().update({
-//   name: 'Mike',
-//   age: 29,
-//   job: 'Software developer',
-//   isSingle: null
+// database.ref('notes/-L_hSVK5t6sEXKRGj5ii').update({
+//   body: 'Buy food'
 // });
 
-// to update nested data instead of root we need to use a different syntax
-// change teh stressLevel to a 9
-// Change job.company to Amazon
-// Change location.city to Seattle
-// database.ref().update({
-//   stressLevel: 9,
-//   'job/company': 'Amazon',
-//   'location/city': 'Seattle'
+// database.ref('notes/-L_hSVK5t6sEXKRGj5ii').remove();
+
+// push() method is used to create a list with a uniqure property for every element pushed 
+// and also returns its reference
+// database.ref('notes').push({
+//   title: 'Course Topics',
+//   body: 'React, Native, Angular, Python'
+// });
+
+// const firebaseNotes = {
+//   notes: {
+//     apsdfs: {
+//       title: 'First note!',
+//       body: 'This is my note'
+//     },
+//     asfsfsdfds: {
+//       title: 'Another note',
+//       body: 'This is my note'
+//     }
+//   }
+// };
+
+// const notes = [
+//   { id: '12', title: 'First note!', body: 'This is my note' },
+//   { id: '1322', title: 'Another note!', body: 'This is my note' }
+// ];
+
+// database.ref('notes').set(notes);
+
+// Setup data sub -> Andrew is a software Developer at amazon
+// const onValueChange = database.ref().on('value', (snapshot) => {
+//   const value = snapshot.val();
+//   console.log(`${value.name} is a ${value.job.title} at ${value.location.city}` );
+// }, (e) => {
+//   console.log('Data Fetching failed', e);
 // });
