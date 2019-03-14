12fdf10a56cd651c97f5eb097cccb28ec27b0764
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2019-03-14 13:27:31 -0400

    163. Loggin Out - Setup and testing

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import {shallow } from 'enzyme';
+import { shallow } from 'enzyme';
 // this lib is used to get only the rendering part in snapchat and not the extras
 import toJSON from 'enzyme-to-json';
 // import ReactShallowRenderer from 'react-test-renderer/shallow';
-import Header from '../../components/Header';
+import { Header } from '../../components/Header';
 
 test('should render Header correctly', () => {
-  const wrapper = shallow(<Header />);
+  const wrapper = shallow(<Header startLogout={() => {}} />);
   expect(wrapper).toMatchSnapshot();
+});
 
-  //expect(wrapper.find('h1').text()).toBe('Expensify');
-  // const renderer = new ReactShallowRenderer();
-  // renderer.render(<Header />);
-  // expect(renderer.getRenderOutput()).toMatchSnapshot();
+test('should call startLogout on button click', () => {
+  const startLogout = jest.fn();
+  const wrapper = shallow(<Header startLogout={startLogout} />);
+  wrapper.find('button').simulate('click');
+  expect(startLogout).toHaveBeenCalled();
 });
